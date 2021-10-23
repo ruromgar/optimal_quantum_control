1413304b9993cf70bdc249466dfad864c15140f5
Ruben <ruromgar@gmail.com>
2021-10-23 11:05:46 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from src.optimal_quantum_control import OptimalQuantumControl
+
+control = OptimalQuantumControl(None, None, None, None, True)
+
+control.unitary_grape()
+control.fidelity()
+control.control()
+control.grape_pulse()
